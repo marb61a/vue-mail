56b68b9707eafcc0d3eb69481fb1983048fa11be
martin byrne <marb61a@gmail.com>
2017-11-21 13:56:49 +0000

    added initial Sidebar and Compose vue files

---
+++ src/Compose.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Compose
+    </div>
+</template>
+<script>
+    import moment from 'moment';
+    import { eventBus } from './main';
+    
+    export default {
+        
+    }
+</script>
---
+++ src/Sidebar.vue
@@ -0,0 +1,22 @@
+<template>
+    <aside class="sm-side">
+        <div class="user-head">
+            <img src="src/assets/images/profile.jpg">
+            <div class="user-name">
+                <h5>Joe Bloggs</h5>
+                <span class="email-address">joebloggs@example.com</span>
+            </div>
+        </div>
+        <div class="compose-wrapper">
+            <app-compose></app-compose>
+        </div>
+    </aside>
+</template>
+<script>
+    import { eventBus } from './main';
+    import Compose from './Compose.vue';
+    
+    export default {
+    
+    }
+</script>
