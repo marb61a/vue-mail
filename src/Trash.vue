c410aba44cbf6cb3b658cf40c454ef19cf4981a6
martin byrne <marb61a@gmail.com>
2017-11-22 17:38:11 +0000

    added initial sent and trash vue files

---
+++ src/Trash.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="inbox-body">
+        <app-messages :messages="trashedMessages"></app-messages>
+    </div>
+</template>
+<script>
+    import Messages from './Messages.vue';
+    
+    export default {
+        props: {
+            data: {
+                
+            }
+        }
+    };
+</script>
