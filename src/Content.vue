ee04bac1c0e3764e859d7ed48770ead4a0265e37
martin byrne <marb61a@gmail.com>
2017-11-22 17:30:15 +0000

    added initial content and messages vue files

---
+++ src/Content.vue
@@ -0,0 +1,14 @@
+<template>
+    <aside class="lg-side">
+        
+    </aside>
+</template>
+<script>
+    import { eventBus } from './main';
+    import Inbox from './Inbox.vue';
+    import Sent from './Sent.vue';
+    import Important from './Important.vue';
+    import Trash from './Trash.vue';
+    import ViewMessage from './ViewMessage.vue';
+    
+</script>
---
+++ src/Messages.vue
@@ -0,0 +1,16 @@
+<template>
+    <table>
+        <tbody>
+            
+        </tbody>    
+    </table>
+</template>
+<script>
+    import { eventBus } from './main';
+    
+    export default {
+        props: {
+            
+        }    
+    };
+</script>
