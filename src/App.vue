b05e11f09080ecd35781c5aa6cb275a161355970
martin byrne <marb61a@gmail.com>
2017-11-24 12:22:40 +0000

    added initial style and updated app vue file

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <tmeplate>
   <div class="container">
     <div class="mail-box">
-      
+      <app-sidebar :messages="messages"></app-sidebar>
+      <app-content :messages="messages"></app-content>  
     </div>
   </div>
 </tmeplate>
 <script>
+  import Sidebar from './Sidebar.vue';
+  import Content from './Content.vue';
+  import messages from './data/messages';
+  import randomMessages from './data/random-messages';
+  import { eventBus } from './main';
+
   export default {
-    
-  }
+    data(){
+      return {
+        messages: messages
+      };
+    }  
+  };
 </script>
