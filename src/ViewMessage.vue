6c1f8eb701e5984b19a9ade494e6cf1d6e6e1670
martin byrne <marb61a@gmail.com>
2017-11-30 11:30:02 +0000

    added initial viewMessage file

---
+++ src/ViewMessage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="inbox-body">
+        <button class="btn btn-primary" @click="navigateBack">
+            <i class="fa fa-arrow-left" aria-hidden="true"></i>&nbsp; Back
+        </button>
+        
+    </div>
+</template>
+
+<script>
+    import { eventBus } from './main';
+    
+    export default {
+        props: {
+            data: {
+                type: Object,
+                required: true
+            }
+        },
+        methods: {
+            navigateBack(){
+                let previousView = this.$parent.previousView;
+                
+                eventBus.$emit('changeView', {
+                    tag: previousView.tag,
+                    title: previousView.title,
+                    data: previousView.data
+                });
+            }
+        }
+    };
+</script>
