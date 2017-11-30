048d3b6dbb6c1d4bcf1dd3d902f9757c1ee1b471
martin byrne <marb61a@gmail.com>
2017-11-30 11:55:50 +0000

    updated viewMessage file

--- src/ViewMessage.vue
+++ src/ViewMessage.vue
@@ -3,7 +3,15 @@
         <button class="btn btn-primary" @click="navigateBack">
             <i class="fa fa-arrow-left" aria-hidden="true"></i>&nbsp; Back
         </button>
+        <button class="btn btn-danger" @click="data.message.isDeleted = true" :disabled="data.message.isDeleted">
+            <i class="fa fa-trash-o"></i>&nbsp; {{ data.message.isDeleted ? 'Deleted' : 'Delete' }}
+        </button>
         
+        <template v-if="typeof data.message.isRead !== 'undefined'">
+            <button class="btn btn-primary">
+                
+            </button>
+        </template>
     </div>
 </template>
 
@@ -26,6 +34,26 @@
                     title: previousView.title,
                     data: previousView.data
                 });
+            }
+        },
+        activated(){
+            if(typeof this.data.message.isRead !== 'undefined'){
+                this.data.message.isRead = true;
+            }    
+        },
+        filters: {
+            formatBytes(bytes){
+                if(bytes == 0){
+                    return '0 Bytes';
+                }
+                
+                let decimals = 2;
+                let k = 1000;
+                let dm =decimals + 1 || 3;
+                let sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+                let i = Math.floor(Math.log(bytes) / Math.log(k));
+                
+                return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
             }
         }
     };
